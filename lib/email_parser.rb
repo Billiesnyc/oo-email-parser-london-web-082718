b9dc6bc1830118b9277a9691db1b7102ea2495b4
Billie Simmons <billiesnyc@gmail.com>
2018-08-31 12:52:54 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -18,7 +18,7 @@ class EmailParser
       split_array = @emails.split(", ")
       split_array.map do |email|
         if email.include?(" ")
-          
+          email.split(" ")
     end
   end
   
